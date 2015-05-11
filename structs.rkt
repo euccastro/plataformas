c167b1113ccc7c7e381be9a679bf6cd116d86efc
manuel <manuel.mayo.lado@gmail.com>
2015-05-11 03:28:37 +0200

    resubida

---
+++ structs.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(struct punto (x y) #:transparent)
+(struct punto-c (x y) #:transparent)
+
+
+(struct pj (estado-m estado-s punto punto-pantalla) #:transparent)
+
+(struct xogo (pj temp vel-c cuadricula cadros-restantes estado carga conm p-mouse) #:transparent)
+
+(provide (struct-out punto))
+(provide (struct-out punto-c))
+(provide (struct-out pj))
+(provide (struct-out xogo))
