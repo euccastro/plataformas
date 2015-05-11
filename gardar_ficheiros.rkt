f5dd8893c0c80152918d12a279307d04c26ee542
manuel <manuel.mayo.lado@gmail.com>
2015-05-11 02:41:34 +0200

    reinicio

--- gardar_ficheiros.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-;;;;FUNCIÓNS PARA GARDAR UN ARQUIVO
-
-;(require racket/file)
-
-;(file->string path [#:mode mode-flag]) → string?
-;  path : path-string?
-;  mode-flag : (or/c 'binary 'text) = 'binary
